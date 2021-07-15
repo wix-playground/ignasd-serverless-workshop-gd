50a7fb21c04c610d3968ed378aa1cf1fee7dc61d
Ignas Daukšas <ignasd@wix.com>
2021-07-15 10:46:37 +0300

    msm call

--- serverless/serverless-demo-ignasd/src/index.ts
+++ serverless/serverless-demo-ignasd/src/index.ts
@@ -1,7 +1,12 @@
 import type { FunctionsBuilder } from '@wix/serverless-api';
 import { FullHttpResponse } from '@wix/serverless-api';
+import { MsmServer } from '@wix/ambassador-msm-server';
 
 module.exports = function builder (builder: FunctionsBuilder) {
   return builder
-    .addWebFunction('GET', '/hello', async () => new FullHttpResponse({ status: 200, body: 'hello, serverless' }));
+    .addWebFunction('GET', '/hello', async () => new FullHttpResponse({ status: 200, body: 'hello, serverless' }))
+    .addWebFunction('GET', '/isfree/:sitename', async (ctx, req) => {
+      const msmServerClient = MsmServer().MetaSiteReadApi()(ctx.aspects);
+      return await msmServerClient.isSiteNameFree({ siteName: req.params.sitename });
+    });
 };
